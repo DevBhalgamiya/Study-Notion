8b19c79bbdc2a73c154d88047d576ae1a7d7e443
Dev Bhalgamiya <146448007+DevBhalgamiya@users.noreply.github.com>
2024-10-04 11:10:58 +0530

    frontend setup with React.js and Tailwind css

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-red-500 w-full h-[100px] text-black text-center'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
